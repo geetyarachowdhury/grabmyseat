4218e2192684b438d77c4a47239feb2948fa3557
Geety <geetyarachowdhury@gmail.com>
2025-12-10 13:58:22 +0530

    Initialised my movie ticket booking app

---
+++ src/main/java/com/geety/grabmyseat/GrabmyseatApplication.java
@@ -0,0 +1,13 @@
+package com.geety.grabmyseat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrabmyseatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrabmyseatApplication.class, args);
+	}
+
+}
